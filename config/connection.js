4dc521e56e7ed308d3fd5fd90749f443a46e5b2e
David Hawkins <uxhawk@gmail.com>
2020-05-03 14:33:20 -0400

    changed connection to var from const for reassignment

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 // Set up MySQL Connection
 // ****************************
 const mysql = require("mysql");
-const connection;
+var connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);

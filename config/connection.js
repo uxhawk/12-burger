b599c35ea90728c90e54d0d6d9727d9940c267d9
David Hawkins <uxhawk@gmail.com>
2020-05-03 14:18:22 -0400

    adeed jaws_db connection

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,20 @@
 // Set up MySQL Connection
 // ****************************
 const mysql = require("mysql");
+const connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
+
 
 // ****************************
 // Make Connection

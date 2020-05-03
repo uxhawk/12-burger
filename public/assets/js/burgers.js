696d12615d951aa847e411631925eee7aa8fea48
David Hawkins <uxhawk@gmail.com>
2020-05-02 22:26:22 -0400

    added ajax interactions from previous projects

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,44 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".set-devoured").on("click", function(event) {
+        var id = $(this).data("id");
+        // var devoured = $(this).data("newsleep");
+
+        var newDevouredState = {
+            devoured: true
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(
+            function() {
+                console.log("changed devoured to", newDevouredState);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function(event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        const newBurger = {
+            name: $("#newBurger").val().trim(),
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("created new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
